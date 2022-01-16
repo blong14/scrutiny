7f4e8c7d5ccd49814a2915c7f1504d7e07f7b824
Ben Long <14benj@gmail.com>
2022-01-16 15:11:23 -0500

    Adding simple pagination

--- library/views.py
+++ library/views.py
@@ -1,3 +1,6 @@
+import logging
+from typing import Any, List
+
 import requests
 from django.conf import settings
 from django.contrib.auth import mixins as auth
@@ -15,6 +18,15 @@ class PocketListView(auth.LoginRequiredMixin, ScrutinyListView):
     pocket_url = "https://getpocket.com/v3/get"
     template_name = "library/list.html"
 
+    @staticmethod
+    def _parse_response(resp) -> List[Any]:
+        items: List[Any] = []
+        try:
+            items = [item for _, item in resp.get("list", {}).items()]
+        except AttributeError as e:
+            logging.warning(e)
+        return items
+
     def get_context_data(self, *args, **kwargs):
         with tracer.start_as_current_span(f"{__name__}.get_context_data"):
             context = super().get_context_data(*args, **kwargs)
@@ -26,10 +38,16 @@ class PocketListView(auth.LoginRequiredMixin, ScrutinyListView):
                 "contentType": "article",
                 "detailType": "complete",
                 "count": 10,
+                "offset": 0,  # zero based
             }
+            page = int(self.request.GET.get("next", 1))
+            data["offset"] = (data["count"] * page) - data["count"]
             resp = requests.post(
                 self.pocket_url, json=data, headers={"Content-Type": "application/json"}
             )
             resp.raise_for_status()
-            context["items"] = [item for _, item in resp.json()["list"].items()]
+            items = self._parse_response(resp.json())
+            context["items"] = items
+            context["previous"] = page - 1 if page > 1 else 0
+            context["next"] = page + 1
             return context
