9eea86eb04e8eecf971e5af486532ecf99e279b5
Ben Long <blong14@lemur.lan1>
2022-02-06 10:17:14 -0500

    Updating library search functionality

--- library/views.py
+++ library/views.py
@@ -11,6 +11,7 @@ from scrutiny.env import get_pocket_consumer_key
 from scrutiny.views import ScrutinyListView
 
 
+logger = logging.getLogger(__name__)
 tracer = trace.get_tracer(__name__)
 
 
@@ -36,16 +37,19 @@ class PocketListView(auth.LoginRequiredMixin, ScrutinyListView):
         with tracer.start_as_current_span(f"{__name__}.get_context_data"):
             context = super().get_context_data(*args, **kwargs)
             user = self.request.user.social_auth.first()
+            page = int(self.request.GET.get("next", 1))
+            search = self.request.GET.get("search", None)
             data = {
                 "consumer_key": get_pocket_consumer_key(),
                 "access_token": user.extra_data.get("access_token", ""),
                 "contentType": "article",
                 "detailType": "complete",
+                "search": search,
                 "count": 10,
                 "offset": 0,  # zero based
             }
-            page = int(self.request.GET.get("next", 1))
             data["offset"] = (data["count"] * page) - data["count"]
+            logger.info(data)
             resp = requests.post(
                 self.pocket_url, json=data, headers={"Content-Type": "application/json"}
             )
@@ -54,4 +58,5 @@ class PocketListView(auth.LoginRequiredMixin, ScrutinyListView):
             context["items"] = items
             context["previous"] = page - 1 if page > 1 else 0
             context["next"] = page + 1
+            context["search"] = search
             return context
