05f18ba2f99aee20eb0183eb414fc1e8e4cc8a7e
Ben Long <blong14@lemur.lan1>
2022-02-17 16:30:07 -0500

    Adding basic search functionality. Fixing feature flag

--- library/views.py
+++ library/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import mixins as auth
+from django.db.models import Q
 from django.views import generic
 
 from library.models import Article, Tag
@@ -11,10 +12,11 @@ class IndexView(auth.LoginRequiredMixin, generic.TemplateView):
 class TagListView(auth.LoginRequiredMixin, generic.ListView):
     context_object_name = "items"
     model = Tag
+    order = ["value"]
     template_name = "library/tags.html"
 
     def get_queryset(self, *args, **kwargs):
-        return self.model.objects.filter(user=self.request.user).all()
+        return self.model.objects.filter(user=self.request.user).order_by(*self.order)
 
 
 class PocketListView(generic.ListView):
@@ -25,7 +27,21 @@ class PocketListView(generic.ListView):
     paginate_by = 10
     template_name = "library/list.html"
 
+    def get_context_data(self, *args, object_list=None, **kwargs):
+        context = super().get_context_data(*args, object_list=object_list, **kwargs)
+        context.pop("object_list")
+        context["tag"] = self.request.GET.get("tag")
+        context["search"] = self.request.GET.get("search")
+        return context
+
     def get_queryset(self):
-        return self.model.objects.filter(user=self.request.user).order_by(
-            *self.order
-        )
+        query = self.model.objects.filter(user=self.request.user).order_by(*self.order)
+        tag = self.request.GET.get("tag")
+        if tag:
+            query = query.filter(tags__value=tag)
+        search = self.request.GET.get("search")
+        if search:
+            query = query.filter(
+                Q(resolved_title__contains=search) | Q(excerpt__contains=search)
+            )
+        return query
