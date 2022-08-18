2571127fe91c5a573f4f80ad6f2fafdffaff2463
Ben Long <blong14@lemur.localdomain>
2022-08-17 21:20:55 -0400

    Removing tracing

--- jobs/views.py
+++ jobs/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import get_object_or_404
 from django.views import generic
-from opentelemetry import trace
 from rest_framework.generics import UpdateAPIView
 
 from jobs.models import Job
 from jobs.serializers import JobSerializer
-
-
-tracer = trace.get_tracer(__name__)
 
 
 class JobsStatusView(generic.TemplateView):
@@ -16,18 +12,14 @@ class JobsStatusView(generic.TemplateView):
     template_name = "jobs/dashboard.html"
 
     def get_context_data(self, *args, **kwargs):
-        with tracer.start_as_current_span(f"{self.module}.get_context_data"):
-            context = super().get_context_data(*args, **kwargs)
-            name = self.request.GET.get("name") if self.request else None
-            if not name:
-                name = kwargs.pop("name")
-            with tracer.start_as_current_span(
-                f"{self.module}.get_context_data.get_object_or_404"
-            ):
-                item = get_object_or_404(self.model, name=name)
-            context["status"] = item.status if item.active else "Degraded"
-            context["last_sync"] = item.synced_at
-            return context
+        context = super().get_context_data(*args, **kwargs)
+        name = self.request.GET.get("name") if self.request else None
+        if not name:
+            name = kwargs.pop("name")
+        item = get_object_or_404(self.model, name=name)
+        context["status"] = item.status if item.active else "Degraded"
+        context["last_sync"] = item.synced_at
+        return context
 
 
 class JobsApiUpdateView(UpdateAPIView):
--- wsgi/management/commands/waitress.py
+++ wsgi/management/commands/waitress.py
@@ -1,51 +1,16 @@
-import functools
 import logging
 import time
 from typing import Callable
 
 from django.core.management.base import BaseCommand
 from django.utils.timezone import now as utc_now
-from opentelemetry import context, trace
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-from opentelemetry.instrumentation.wsgi import (
-    OpenTelemetryMiddleware,
-    collect_request_attributes,
-    get_default_span_name,
-    wsgi_getter,
-)
-from opentelemetry.propagate import extract
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
-from opentelemetry.trace.status import Status, StatusCode
 
 from waitress import serve
 
-from scrutiny import env
 from scrutiny.wsgi import application
 
 
 logger = logging.getLogger(__name__)
-provider = TracerProvider(
-    resource=Resource.create({SERVICE_NAME: "scrutiny-http-service"})
-)
-trace.set_tracer_provider(provider)
-trace.get_tracer_provider().add_span_processor(
-    BatchSpanProcessor(
-        JaegerExporter(
-            agent_host_name="jaeger",
-            agent_port=6831,
-        )
-        if env.should_trace()
-        else InMemorySpanExporter(),
-    )
-)
-
-
-def _default_span_name(environ):
-    _, _, uri = _request_info(environ)
-    return f"{get_default_span_name(environ)} {uri}"
 
 
 def _request_info(environ):
@@ -53,55 +18,6 @@ def _request_info(environ):
     if environ.get("QUERY_STRING"):
         req_uri = f"{req_uri}?{environ['QUERY_STRING']}"
     return environ.get("SERVER_PROTOCOL"), environ.get("REQUEST_METHOD"), req_uri
-
-
-# Put this in a sub function to not delay the call to the wrapped
-# WSGI application (instrumentation should change the application
-# behavior as little as possible).
-def _end_span_after_iterating(iterable, span, token):
-    try:
-        with trace.use_span(span):
-            yield from iterable
-    finally:
-        close = getattr(iterable, "close", None)
-        if close:
-            close()
-        span.end()
-        context.detach(token)
-
-
-class _TracingApplication(OpenTelemetryMiddleware):
-    def __call__(self, environ, start_response):
-        """The WSGI application
-        Args:
-            environ: A WSGI environment.
-            start_response: The WSGI start_response callable.
-        """
-        token = context.attach(extract(environ, getter=wsgi_getter))
-        _, method, uri = _request_info(environ)
-        span = self.tracer.start_span(
-            _default_span_name(environ),
-            kind=trace.SpanKind.SERVER,
-            attributes=collect_request_attributes(environ),
-        )
-        if self.request_hook:
-            self.request_hook(span, environ)
-        response_hook = self.response_hook
-        if response_hook:
-            response_hook = functools.partial(response_hook, span, environ)
-        try:
-            with trace.use_span(span):
-                start_response = self._create_start_response(
-                    span, start_response, response_hook
-                )
-                iterable = self.wsgi(environ, start_response)
-                return _end_span_after_iterating(iterable, span, token)
-        except Exception as ex:
-            if span.is_recording():
-                span.set_status(Status(StatusCode.ERROR, str(ex)))
-            span.end()
-            context.detach(token)
-            raise
 
 
 class _LoggingApplication:
