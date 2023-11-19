543089e760d1db5ef79ec054a763db4ddfa03945
Ben Long <blong14@lemur.localdomain>
2023-11-19 12:41:53 -0500

    Adding ordering

--- jobs/models.py
+++ jobs/models.py
@@ -13,5 +13,8 @@ class Job(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ('-created_at',)
+
     def __str__(self):
         return self.name
